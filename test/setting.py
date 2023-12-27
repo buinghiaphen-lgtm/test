54123607f3fbc41581d64b2feb53153a58aa8159
lingling wang <352003017@qq.com>
2023-12-27 11:32:49 +0800

    部署到k8s

--- test/setting.py
+++ test/setting.py
@@ -17,14 +17,14 @@ KAFKA_TNAME = []
 KAFKA_SERVER = os.getenv('KAFKA_SERVER') or '192.168.24.213:31050'
 
 # 数据库连接地址
-# DB_HOST = os.getenv('DB_HOST') or '192.168.24.210'
-# DB_PORT = os.getenv('DB_PORT') or 4000
-# DB_USER = os.getenv('DB_USER') or 'v2t'
-# DB_PASSWORD = os.getenv('DB_PASSWORD') or 'Vegas2.0'
-# DB_NAME = os.getenv('DB_NAME') or 'test_vegas2'
-
-DB_HOST = os.getenv('DB_HOST') or '192.168.24.200'
-DB_PORT = os.getenv('DB_PORT') or 3390
-DB_USER = os.getenv('DB_USER') or 'v2t_debug'
+DB_HOST = os.getenv('DB_HOST') or '192.168.24.210'
+DB_PORT = os.getenv('DB_PORT') or 4000
+DB_USER = os.getenv('DB_USER') or 'v2t'
 DB_PASSWORD = os.getenv('DB_PASSWORD') or 'Vegas2.0'
-DB_NAME = os.getenv('DB_NAME') or 'debug_vegas2'
+DB_NAME = os.getenv('DB_NAME') or 'test_vegas2'
+
+# DB_HOST = os.getenv('DB_HOST') or '192.168.24.200'
+# DB_PORT = os.getenv('DB_PORT') or 3390
+# DB_USER = os.getenv('DB_USER') or 'v2t_debug'
+# DB_PASSWORD = os.getenv('DB_PASSWORD') or 'Vegas2.0'
+# DB_NAME = os.getenv('DB_NAME') or 'debug_vegas2'
