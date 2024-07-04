5a53883ba5084065855eb55d2474cf01458c0666
wllling <352003017@qq.com>
2024-07-04 11:45:29 +0800

    setting.py修改端口

--- test/setting.py
+++ test/setting.py
@@ -8,17 +8,17 @@ TEST_DATA_PATH = Path(BASE_DIR, 'data', '测试数据-实时推送-kafka工具.x
 
 # REDIS相关配置
 REDIS_HOST = os.getenv('REDIS_HOST') or '192.168.24.213'
-REDIS_PORT = os.getenv('REDIS_PORT') or 59229
+REDIS_PORT = os.getenv('REDIS_PORT') or 30008
 REDIS_DB = os.getenv('REDIS_DB') or 0
 REDIS_HNAME = os.getenv('REDIS_HNAME') or 'kafkaLastTime'
 
 # KAFKA相关配置
 KAFKA_TNAME = []
-KAFKA_SERVER = os.getenv('KAFKA_SERVER') or '192.168.24.213:31050'
+KAFKA_SERVER = os.getenv('KAFKA_SERVER') or '192.168.24.213:30014'
 
 # 数据库连接地址
 DB_HOST = os.getenv('DB_HOST') or '192.168.24.210'
-DB_PORT = os.getenv('DB_PORT') or 4000
+DB_PORT = os.getenv('DB_PORT') or 3390
 DB_USER = os.getenv('DB_USER') or 'v2t'
 DB_PASSWORD = os.getenv('DB_PASSWORD') or 'Vegas2.0'
 DB_NAME = os.getenv('DB_NAME') or 'test_vegas2'
